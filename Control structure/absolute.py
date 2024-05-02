7bcae562d3a0943c84170db69dfdc2e040300321
James Okonita <118991124+Oluwafemi5@users.noreply.github.com>
2024-05-02 14:00:06 +0100

    absolute.py

---
+++ Control structure/absolute.py
@@ -0,0 +1,6 @@
+'This program will execute the absolute number given in the program'
+number= int(input('Pls enter your number '))
+result = abs(number)
+print(f'the absolute number of {number} is {result}')
+
+'I used a absolute function in the program sir '
