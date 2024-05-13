6d03b4245a3728ba21caff8d2cbc90dfbf6ddde9
James Okonita <118991124+Oluwafemi5@users.noreply.github.com>
2024-05-13 12:47:22 +0100

    area_rectangle.py

---
+++ funtion/area_rectangle.py
@@ -0,0 +1,6 @@
+def calculate_area_rectangle ():
+  length = int(input ("Pls enter you length:  "))
+  width = int(input ("pls enter your width: "))
+  result = f' the area of you rectangle is {length * width}' 
+  return result
+print (calculate_area_rectangle ())
