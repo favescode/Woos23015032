14ef0101f62cdf10c5b57050e8162e920cf19665
James Okonita <118991124+Oluwafemi5@users.noreply.github.com>
2024-05-24 12:28:54 +0100

    find_max_min.py

--- Data structure/find_max_min.py
+++ Data structure/find_max_min.py
@@ -1,20 +1,14 @@
 
 def find_max_min():
     nums = input("Please enter some numbers separated by spaces: ").split()
-
     # Convert input strings to integers
     nums = [int(num) for num in nums]
-
     if not nums:
         print("No numbers were entered.")
         return None
-
     max_num = max(nums)
     min_num = min(nums)
-
     return max_num, min_num
-
-
 result = find_max_min()
 if result:
     max_num, min_num = result
